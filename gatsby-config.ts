d50f2b595f2d8e3fd62099368efbab6ce50094e1
Sina Jamshidi <hello@sinajamshidi.com>
2024-11-11 19:17:17 -0800

    update metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `sina-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `sina-jamshidi-portfolio`,
+    siteUrl: `https://sinajamshidi.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
